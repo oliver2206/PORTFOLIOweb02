c999236b2118dea4c38b6da8d7628fc4b6108c45
oliver2206 <oliveriinathanieltolentino@gmail.com>
2026-04-30 12:59:54 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -339,8 +339,8 @@ const GLOBAL_STYLES = `
     top: 50%;
     left: 50%;
     transform: translate(-50%, -50%);
-    min-width: 100%;
-    min-height: 100%;
+    min-width: 120%;
+    min-height: 120%;
     width: auto;
     height: auto;
     overflow: hidden;
