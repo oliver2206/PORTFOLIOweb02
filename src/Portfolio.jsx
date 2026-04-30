c8d450a1ac280a893b973c8c2e11d4738ca616b0
oliver2206 <oliveriinathanieltolentino@gmail.com>
2026-04-30 22:54:54 +0800

    Update Portfolio.jsx

--- src/Portfolio.jsx
+++ src/Portfolio.jsx
@@ -1,6 +1,5 @@
 import { useState, useEffect, useRef } from 'react';
 import About from './About';
-import Contact from './Contact'; 
 import Portfolio from './Portfolio';
 
 const heroVideo = '/videos/virtual-tour-vigan.mp4';
